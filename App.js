a04b659d5f34759e99d075439761f338ca4b99d5
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-05-22 20:31:34 +0300

    Add TextInput and button

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default class App extends React.Component {
+  state = {
+    placeName: "",
+    places: []
+  };
+  placeSubmitHandler = () => {
+    const placeNameEntered = this.state.placeName;
+    if (placeNameEntered.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
+  placeNameChangeHandler = val => {
+    this.setState({ placeName: val });
+  };
   render() {
+    const placesOutput = this.state.places.map((place, index) => <Text key={index}>{place}</Text>);
     return (
       <View style={styles.container}>
-        <Text>Lets go</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{
+              width: 300,
+              height: 40
+            }}
+            placeholder="An awesome place"
+            onChangeText={this.placeNameChangeHandler}
+            value={this.state.placeName}
+          />
+          <Button
+            onPress={this.placeSubmitHandler}
+            title="Add"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+        <View>{placesOutput}</View>
       </View>
     );
   }
@@ -13,9 +50,15 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 26,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
 });
+
